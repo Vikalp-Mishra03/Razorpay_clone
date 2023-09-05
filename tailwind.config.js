aa600722ef3f5cd2f4d4109270979416364d1157
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-05 06:37:18 +0530

    hero section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ["Mullish", "sans-serif"],
+    },
+    colors: {
+      deepblue: "#02042a",
+      lightblue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepblueHead: "#162f56",
+      gray2: "#525a76",
+      white: "#ffffff"
+    }
   },
   plugins: [],
 }
