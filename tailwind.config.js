1fa353238c20eb13f1d85406d87b0361a81dab75
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-13 07:57:02 +0530

    join section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
       deepblueHead: "#162f56",
       gray2: "#525a76",
       white: "#ffffff",
-      gray3: "hsla(229, 9.6%, 54.9%, 1)"
+      gray3: "hsla(229, 9.6%, 54.9%, 1)",
+      darkgray: "hsla(220, 81.8%, 97.8%, 1)"
     }
   },
   plugins: [],
